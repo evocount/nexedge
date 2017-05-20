d47147d57872599762b3256d8285cb0df430ab95
Suthep Pomjaksilp <git@laz0r.de>
2017-05-20 14:14:22 +0200

    class based approach works. moved testing to seperate file.

--- main_radio.py
+++ main_radio.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import json
-import time
 import serial
 import serial.threaded
 import threading
@@ -26,50 +25,6 @@ def split_to_chunks(data: bytes, chunksize: int):
         yield data[i:i + chunksize]
 
 
-testnachricht = "Hallo Welt hier spricht EvoCount"
-
-# load serial configuration from json formatted file
-with open("config.json", "r") as fpointer:
-    config = json.load(fpointer)
-
-ser = serial.Serial(config["serial"]["port"])  # as str in config.json
-ser.baudrate = config["serial"]["baudrate"]  # as int in config.json
-ser.parity = eval(config["serial"]["parity"])  # we have to evaluate the code in the file
-ser.stopbits = eval(config["serial"]["stopbits"])
-ser.bytesize = eval(config["serial"]["bytesize"])
-
-# Setting up queueing
-# sender_q = Queue(maxsize=0)
-#
-# # Setting up th reader thread
-# protocol = serial.threaded.ReaderThread(ser, receiver.NexedgePacketizer)
-#
-# with protocol as receiver:
-#     # Setting up sender thread
-#     sender_thread = threading.Thread(target=sender.send_worker,
-#                                      args=(sender_q, protocol, receiver.channel_status, receiver.transmission_queue,)
-#                                      )
-#     sender_thread.setDaemon(True)
-#     sender_thread.start()
-#
-#     # queue up a message
-#     sender_q.put(longMessage2Unit(b"00011", testnachricht.encode()))
-#
-#     # loop until the answer_queue is None, this never happens
-#     for m in iter(receiver.answer_queue.get, None):
-#         # if we got a message, reply!
-#         if "hallo" not in m.decode():  # do not produce a infinite loop
-#             # 3 items to check if sending part works with multiple items in queue
-#             sender_q.put(("\x02" + "g" + "G" + "U" + "00011" + "hallo alice" + "\x03").encode())
-#             sender_q.put(("\x02" + "g" + "G" + "U" + "00011" + "hallo bob" + "\x03").encode())
-#             sender_q.put(("\x02" + "g" + "G" + "U" + "00011" + "hallo carol" + "\x03").encode())
-#
-#         # debugging output
-#         print("message: {}". format(m.decode()))
-#         print("channel free: {}".format(receiver.channel_status.is_free()))
-#         print("queue size: answer={}\tsender={}".format(receiver.answer_queue.qsize(), sender_q.qsize()))
-
-
 class Radio(object):
     # setting up sender queue
     sender_queue = Queue(maxsize=0)
@@ -77,8 +32,8 @@ class Radio(object):
     # setting up data queue, json data goes here
     data_queue = Queue(maxsize=0)
 
-    def __init__(self, ser: serial.Serial, max_chunk_size: int=4096):
-        self.serial_connection = ser
+    def __init__(self, serialcon: serial.Serial, max_chunk_size: int=4096):
+        self.serial_connection = serialcon
         self.max_chunk_size = max_chunk_size
 
         # Setting up th reader thread
@@ -112,6 +67,12 @@ class Radio(object):
                                              )
         self.unite_thread.setDaemon(True)
         self.unite_thread.start()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.protocol.__exit__()
 
     def send(self, data: dict, target: bytes):
         # get str representation of data
---
+++ test_radio.py
@@ -0,0 +1,22 @@
+from main_radio import Radio
+import time
+import serial
+import json
+
+testnachricht = "Hallo Welt hier spricht EvoCount"
+
+# load serial configuration from json formatted file
+with open("config.json", "r") as fpointer:
+    config = json.load(fpointer)
+
+ser = serial.Serial(config["serial"]["port"])  # as str in config.json
+ser.baudrate = config["serial"]["baudrate"]  # as int in config.json
+ser.parity = eval(config["serial"]["parity"])  # we have to evaluate the code in the file
+ser.stopbits = eval(config["serial"]["stopbits"])
+ser.bytesize = eval(config["serial"]["bytesize"])
+
+radio = Radio(serialcon=ser, max_chunk_size=4096)
+
+while True:
+    print(radio.get())
+    time.sleep(2)
