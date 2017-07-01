a230bb0c159780676c1a3ed5d158c878b6ca6e50
Suthep Pomjaksilp <git@laz0r.de>
2017-07-01 13:59:03 +0200

    testing works.

--- compression_effiency.py
+++ compression_effiency.py
@@ -14,13 +14,14 @@ import time
 import serial
 import json
 from concurrent.futures import as_completed
+import numpy as np
 
 # some long ass string
 long_str = " in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto "
 long_json_str = '{"id": "' + long_str + '"}'
 
 # testing increment
-inc = 100
+inc = 1000
 
 
 # generate varying test string lenghts
@@ -36,7 +37,17 @@ def chop_str(long_str, inc = 10):
         yield len(test_json_str), test_json
 
 
-def run_test(ser, compression):
+def run_test(compression):
+    # load serial configuration from json formatted file
+    with open("radio_settings.json", "r") as fpointer:
+        config = json.load(fpointer)
+
+    ser = serial.Serial(config["serial"]["port"])  # as str in radio_settings.json
+    ser.baudrate = config["serial"]["baudrate"]  # as int in radio_settings.json
+    ser.parity = eval(config["serial"]["parity"])  # we have to evaluate the code in the file
+    ser.stopbits = eval(config["serial"]["stopbits"])
+    ser.bytesize = eval(config["serial"]["bytesize"])
+
     with Radio(serialcon=ser,
                max_chunk_size=4096,
                compression=compression) as radio:
@@ -64,20 +75,19 @@ def run_test(ser, compression):
         return x, y
 
 
-# load serial configuration from json formatted file
-with open("radio_settings.json", "r") as fpointer:
-    config = json.load(fpointer)
-
-ser = serial.Serial(config["serial"]["port"])  # as str in radio_settings.json
-ser.baudrate = config["serial"]["baudrate"]  # as int in radio_settings.json
-ser.parity = eval(config["serial"]["parity"])  # we have to evaluate the code in the file
-ser.stopbits = eval(config["serial"]["stopbits"])
-ser.bytesize = eval(config["serial"]["bytesize"])
-
-
 # baseline without compression
-x_base, y_base = run_test(ser, False)
+x_base, y_base = run_test(False)
 
 # test with compression enabled
-x_comp, y_comp = run_test(ser, True)
+x_comp, y_comp = run_test(True)
 
+
+# convert to numpy
+
+x_base = np.array(x_base)
+y_base = np.array(y_base)
+
+x_comp = np.array(x_comp)
+y_comp = np.array(y_comp)
+
+print(y_comp-y_base)
