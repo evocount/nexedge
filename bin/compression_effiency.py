5829596e4cbcc964627955838df5880a5d7d9b5a
Suthep Pomjaksilp <git@laz0r.de>
2017-07-11 07:29:40 +0200

    added scripts to bin directory.

---
+++ bin/compression_effiency.py
@@ -0,0 +1,93 @@
+"""
+Copyright (C) EvoCount UG - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+
+Suthep Pomjaksilp <sp@laz0r.de> 2017
+"""
+
+from radio import Radio
+import receiver
+import sender
+from custom_exceptions import *
+import time
+import serial
+import json
+from concurrent.futures import as_completed
+import numpy as np
+
+# some long ass string
+long_str = " in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto "
+long_json_str = '{"id": "' + long_str + '"}'
+
+# testing increment
+inc = 1000
+
+
+# generate varying test string lenghts
+def chop_str(long_str, inc = 10):
+    n = 0
+    while len(long_str) > n*inc:
+        test_str = long_str[:-(n*inc+1)]
+        test_json_str = '{"id": "' + test_str + '"}'
+        test_json = json.loads(test_json_str)
+
+        n += 1
+
+        yield len(test_json_str), test_json
+
+
+def run_test(compression):
+    # load serial configuration from json formatted file
+    with open("radio_settings.json", "r") as fpointer:
+        config = json.load(fpointer)
+
+    ser = serial.Serial(config["serial"]["port"])  # as str in radio_settings.json
+    ser.baudrate = config["serial"]["baudrate"]  # as int in radio_settings.json
+    ser.parity = eval(config["serial"]["parity"])  # we have to evaluate the code in the file
+    ser.stopbits = eval(config["serial"]["stopbits"])
+    ser.bytesize = eval(config["serial"]["bytesize"])
+
+    with Radio(serialcon=ser,
+               max_chunk_size=4096,
+               compression=compression) as radio:
+
+        # x-axis
+        x = []
+
+        # y-axis
+        y = []
+
+        for length, test_json in chop_str(long_str, inc):
+            x.append(length)
+
+            time_start = time.time()
+
+            future_send = radio.send(test_json, b'00011')
+            future_send.result()
+
+            time_end = time.time()
+
+            delta_time = time_end - time_start
+
+            y.append(delta_time)
+
+        return x, y
+
+
+# baseline without compression
+x_base, y_base = run_test(False)
+
+# test with compression enabled
+x_comp, y_comp = run_test(True)
+
+
+# convert to numpy
+
+x_base = np.array(x_base)
+y_base = np.array(y_base)
+
+x_comp = np.array(x_comp)
+y_comp = np.array(y_comp)
+
+print(y_comp-y_base)
