b20bc313a51f907068655d854743d6446c2b0f64
Suthep Pomjaksilp <git@laz0r.de>
2018-07-30 11:27:34 +0200

    updating the documentation nexedge/channel.py

--- nexedge/channel.py
+++ nexedge/channel.py
@@ -13,9 +13,11 @@ import logging
 # logging setup
 logger = logging.getLogger(__name__)
 
+
 class ChannelStatus:
     """
-    This class contains the status of the radio channel as indicated by the led.
+    This class contains the status of the radio channel as
+    indicated by the led.
     """
     _channel_free = True
     _radio_status = "unknown"
@@ -23,10 +25,15 @@ class ChannelStatus:
     # when did we receive a package for the last time
     _time_last_updated = 0
 
-    def __init__(self, free_threshold: int = 4, force_threshold: int = 20):
+    def __init__(self, free_threshold: int = 4, force_threshold: int = 10):
         """
         Initialize Object.
-        Threshold sets the time in seconds in which the channel has to be clear before it is considered really free.
+        Free Threshold sets the time in seconds in which the channel has to be
+        clear before it is considered really free.
+        In case we do not receive anything from the channel for 10 s, the force
+        threshold forces sets the channel free.
+        This works in our scheme since by design only one unit is sending and
+        there is no concurrency.
         :param free_threshold: int
         """
         self.free_threshold = free_threshold
@@ -41,7 +48,8 @@ class ChannelStatus:
 
     def free(self):
         """
-        True if channel is free (led off) and was free for the last self.free_threshold seconds (default = 2s).
+        True if channel is free (led off) and was free for the last
+        self.free_threshold seconds (default = 4s).
         :return: bool
         """
         # first case, we get enough updates on the channel
@@ -72,7 +80,8 @@ class ChannelStatus:
 
     def set_unfree(self, status):
         """
-        Sets self.channel_free to False and the radio status to a human readable string, the time when the channel goes
+        Sets self.channel_free to False and the radio status to a
+        human readable string, the time when the channel goes
         unfree is stored.
         :param status: str
         :return:
