af854a1a9d56644e967b6ecb195ef15d164f5064
Till Theato <till.theato@evocount.de>
2019-10-09 14:51:57 +0200

    Typo in setup.py.

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'.
+        'Programming Language :: Python :: Implementation :: PyPy',
 
         'Topic :: Communications'
     ],
