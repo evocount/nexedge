1db057e9df1dc8e4cf9dc0f33c2ef91603e8a3d1
Till Theato <till.theato@evocount.de>
2019-10-09 14:46:34 +0200

    Extended setup.py.

--- setup.py
+++ setup.py
@@ -1,20 +1,129 @@
-from setuptools import setup
+# adapted from https://github.com/kennethreitz/setup.py/blob/master/setup.py
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+NAME = 'nexedge'
+MODULE = NAME
+DESCRIPTION = 'Kenwood Nexedge communication'
+URL = 'https://github.com/evocount/nexedge'
+EMAIL = 'sp@laz0r.de'
+AUTHOR = 'Suthep Pomjaksilp'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'pyserial-asyncio'
+]
+
+EXTRAS = {
+    'test': [
+        'pytest',
+        'pytest-cov',
+        'pytest-asyncio',
+        'pytest-mock',
+        'pytest-xdist'
+    ],
+}
 
 
-setup(name='nexedge',
-      version='0.2',
-      description='Kenwood Nexedge communication',
-      url='https://github.com/evocount/nexedge',
-      author='Suthep Pomjaksilp',
-      author_email='sp@laz0r.de',
-      license='MIT',
-      packages=['nexedge'],
-      install_requires=[
-          'pyserial-asyncio',
-      ],
+here = os.path.abspath(os.path.dirname(__file__))
 
-      zip_safe=False)
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, MODULE, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'.
+
+        'Topic :: Communications'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
